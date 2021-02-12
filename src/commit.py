1db0f8ebc5f021e18d72160c495a9024f9b40f86
Thomas <Thomas.brefeld@gmail.com>
2021-02-12 20:02:59 +0000

    added git control

--- src/commit.py
+++ src/commit.py
@@ -1,4 +1,5 @@
 import os
+import git
 import json
 
 dataFile = "/home/ubuntu/catkin_ws/src/river/src/data.json"
@@ -9,7 +10,21 @@ with open(dataFile) as file:
 print(data)
 
 with open("remote-master.sh", "w") as file:
-        file.write("export ROS_MASTER_URI=http://" + str(data["settings"]["ros"]["coreIP"]) + ":" + str(data["settings"]["ros"]["port"]))
+	file.write("export ROS_MASTER_URI=http://" + str(data["settings"]["ros"]["coreIP"]) + ":" + str(data["settings"]["ros"]["port"]))
 	file.write("\nexport ROS_IP=" + str(data["settings"]["ros"]["riverIP"]))
 
+#def get_git_root(path):
 
+#        git_repo = git.Repo(path, search_parent_directories=True)
+#        git_root = git_repo.git.rev_parse("--show-toplevel")
+#        print (git_root)
+
+#get_git_root("/home/ubuntu/catkin_ws/src/river/src/data.json")
+
+try:
+	repo = git.Repo(localpath_to_repo_dir)
+	repo.remotes.origin.pull('main')
+	print("done")
+except:
+	print("failed")
+#print(o.pull())
--- src/remote-master.sh
+++ src/remote-master.sh
@@ -1,2 +1,2 @@
 export ROS_MASTER_URI=http://192.168.1.32:11311
-export ROS_IP=http://192.168.1.38
+export ROS_IP=192.168.1.38
