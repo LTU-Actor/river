208e9845fc967d12924cbce71752fbae9ed6c689
Thomas <Thomas.brefeld@gmail.com>
2021-02-12 15:32:47 -0500

    update commit

--- src/commit.py
+++ src/commit.py
@@ -2,24 +2,14 @@ import os
 import git
 import json
 
-dataFile = "/home/ubuntu/catkin_ws/src/river/src/data.json"
+dataFile = str(os.path.normpath(os.getcwd() + os.sep + os.pardir)) + "/src/data.json"
 
 with open(dataFile) as file:
 	data = json.load(file)
 
-print(data)
-
 with open("remote-master.sh", "w") as file:
 	file.write("export ROS_MASTER_URI=http://" + str(data["settings"]["ros"]["coreIP"]) + ":" + str(data["settings"]["ros"]["port"]))
 	file.write("\nexport ROS_IP=" + str(data["settings"]["ros"]["riverIP"]))
-
-#def get_git_root(path):
-
-#        git_repo = git.Repo(path, search_parent_directories=True)
-#        git_root = git_repo.git.rev_parse("--show-toplevel")
-#        print (git_root)
-
-#get_git_root("/home/ubuntu/catkin_ws/src/river/src/data.json")
 
 def git_pull_change(path):
 	repo = git.Repo(path)
@@ -34,7 +24,4 @@ def git_pull_change(path):
 		print("Repo changed! Activated.")
 		return True
 
-print(git_pull_change("/home/ubuntu/catkin_ws/src/river"))
-#print(o.pull())
-print(os.path.normpath(os.getcwd() + os.sep + os.pardir))
-#test4
+git_pull_change(str(os.path.normpath(os.getcwd() + os.sep + os.pardir)))
