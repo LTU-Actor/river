b90349441d55fab6fb6ee029d9962d183529b986
Thomas <Thomas.brefeld@gmail.com>
2021-02-14 18:00:28 -0500

    2

--- src/startUp.py
+++ src/startUp.py
@@ -1,2 +1,49 @@
-#test!
-print("startUp")
+import os
+import sys
+import time
+import signal
+import rosgraph
+import subprocess
+
+class timeout:
+    def __init__(self, seconds=1, error_message='Timeout'):
+        self.seconds = seconds
+        self.error_message = error_message
+    def handle_timeout(self, signum, frame):
+        raise RuntimeError(self.error_message)
+    def __enter__(self):
+        signal.signal(signal.SIGALRM, self.handle_timeout)
+        signal.alarm(self.seconds)
+    def __exit__(self, type, value, traceback):
+        signal.alarm(0)
+
+def isROS():
+        with timeout(1):
+            return rosgraph.is_master_online()
+
+mainProcess = None
+rosProcess = None
+
+try:
+    while True:
+            if isROS():
+                if mainProcess is None:
+                    print("ROS River running.")
+                    mainProcess = subprocess.Popen('exec rosrun river main.py', stdout = subprocess.PIPE, shell = True)
+            else:
+                if not mainProcess is None:
+                    print("Waiting for ROS core.")
+                    try:
+                        mainProcess.kill()
+                    except:
+                        pass
+                    mainProcess = None
+            time.sleep(.1)
+except Exception as e:
+    print("Error:", e)
+except KeyboardInterrupt:
+    print("keyboard Interrupt")
+finally:
+    if not mainProcess is None:
+        mainProcess.kill()
+    sys.exit(0)
