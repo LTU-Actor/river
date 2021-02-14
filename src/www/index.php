adc3bcfdccfd40c4a7054bdc9b95348c3bdae8f1
Thomas <Thomas.brefeld@gmail.com>
2021-02-14 15:30:41 -0500

    1

--- src/www/index.php
+++ src/www/index.php
@@ -64,7 +64,7 @@ if (isset($_POST['show-host-IP'])){
 #}
 if (count($_POST) > 0 && isset($_POST['show-reboot-submit'])){
 	echo "reboot";
-	unset($_REQUEST);
+	unset($_POST);
 	#shell_exec('sudo /sbin/reboot');
 }
 
