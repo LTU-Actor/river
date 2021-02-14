6d08620eff6749e3abdaf234743e397954416423
Thomas <Thomas.brefeld@gmail.com>
2021-02-14 15:29:50 -0500

    1

--- src/www/index.php
+++ src/www/index.php
@@ -54,19 +54,19 @@ if (isset($_POST['show-host-IP'])){
         }
 }
 
-session_start();
-if(isset($_SESSION['show-reboot-submit'])){
-	echo $_SESSION['show-reboot-submit'];
-}
-else{
-	$_SESSION['show-reboot-submit'] = "processed";
-	#shell_exec('sudo /sbin/reboot');
-}
-#if (count($_POST) > 0 && isset($_POST['show-reboot-submit'])){
-#	echo "reboot";
-#	unset($_POST['show-reboot-submit']);
+#session_start();
+#if(isset($_SESSION['show-reboot-submit'])){
+#	echo $_SESSION['show-reboot-submit'];
+#}
+#else{
+#	$_SESSION['show-reboot-submit'] = "processed";
 #	#shell_exec('sudo /sbin/reboot');
 #}
+if (count($_POST) > 0 && isset($_POST['show-reboot-submit'])){
+	echo "reboot";
+	unset($_REQUEST);
+	#shell_exec('sudo /sbin/reboot');
+}
 
 ?>
 
