11f917c93f99d02d3ebc06c77518429656c70255
Thomas <Thomas.brefeld@gmail.com>
2021-02-14 14:38:50 -0500

    1

--- src/www/index.php
+++ src/www/index.php
@@ -58,6 +58,8 @@ if (count($_POST) > 0 && isset($_POST['show-reboot-submit'])){
 	$output = shell_exec('sudo /sbin/reboot');
 	#$output = shell_exec("ls 2>&1");
 	echo $output;
+	$username = posix_getpwuid(posix_geteuid())['name'];
+	echo $username;
 }
 
 ?>
