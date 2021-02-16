5ccefa5e6ec45766bfcbfc14a6974e590d6133b8
Thomas <Thomas.brefeld@gmail.com>
2021-02-16 14:56:05 -0500

    2

--- src/start.sh
+++ src/start.sh
@@ -2,6 +2,11 @@ START_PY_FILE_BIN=/usr/local/bin/startUp.py
 START_PY_FILE_SRC=/home/ubuntu/catkin_ws/src/river/src/startUp.py
 REMOTE_MASTER=/home/ubuntu/catkin_ws/src/river/src/remote-master.sh
 
+while [ "$(hostname -I)" = "" ]; do
+  echo -e "\e[1A\e[KNo network: $(date)"
+  sleep 1
+done
+
 cd "/home/ubuntu/catkin_ws/src/river" && git pull
 
 if test -f "$START_PY_FILE_BIN";
