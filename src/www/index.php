f33211c833a36664ff8ddb5560c040033454291e
Thomas <Thomas.brefeld@gmail.com>
2021-02-17 15:52:56 -0500

    2

--- src/www/index.php
+++ src/www/index.php
@@ -20,7 +20,28 @@
 </head>
 <body>
 <div class="container">
-
+<form action="restart_form.php">
+  <div class="row">
+		<div class="col-25">
+			<label for="fname">Show text</label>
+		</div>
+		<div class="col-75">
+      <input type=text id="fname" name="show-text" placeholder="<?php echo $data['show']['text']['msg']; ?>">
+		</div>
+	</div>
+  <div class="row">
+		<div class="col-25">
+			<label for="fname">Show Status</label>
+		</div>
+		<div class="col-75">
+      <input type=text id="fname" name="show-status" placeholder="<?php echo $data['show']['status']['msg']; ?>">
+		</div>
+	</div>
+  <br>
+	<div class="row">
+		<input type="submit" value="Submit">
+	</div>
+</form>
 </div>
 <div class="container">
   <h3>Settings (Restart Required):</h3>
