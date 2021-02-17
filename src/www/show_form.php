1f289e6d7975a91ebae8c4045ea5a4d9fb6aa945
Thomas <Thomas.brefeld@gmail.com>
2021-02-17 16:19:01 -0500

    2

--- src/www/show_form.php
+++ src/www/show_form.php
@@ -3,17 +3,14 @@
     $strData = file_get_contents($file);
     $data = json_decode($strData, true);
 
-    echo "t";
-    echo $_POST['show-host-IP'];
-    echo $_GET['show-host-IP'];
-    if (isset($_GET['show-host-IP'])){
-        $data["settings"]["ros"]["coreIP"] = $_GET['show-host-IP'];
+    if (isset($_GET['show-text'])){
+        $data["show"]["text"]["msg"] = $_GET['show-text'];
         echo "updated show-host-IP";
     }
-    echo $_GET['show-width'];
-    if (isset($_GET['show-width'])){
-        $data['display']['width'] = $_GET['show-width'];
-        echo "udpated show-width";
+
+    if (isset($_GET['show-status'])){
+        $data["show"]["status"]["msg"] = $_GET['show-status'];
+        echo "udpated show-status";
     }
 
     $jsonData = json_encode($data,JSON_PRETTY_PRINT);
@@ -22,5 +19,5 @@
         echo "Failed";
     }
     fclose($file);
-    #header('Location: http://192.168.1.38/')
+    header('Location: http://192.168.1.38/')
 ?>
