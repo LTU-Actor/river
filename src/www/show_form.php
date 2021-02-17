7c59bd3fd49db2273a798bdddf600ac41dbf5f95
Thomas <Thomas.brefeld@gmail.com>
2021-02-17 16:10:07 -0500

    2

--- src/www/show_form.php
+++ src/www/show_form.php
@@ -3,10 +3,12 @@
     $strData = file_get_contents($file);
     $data = json_decode($strData, true);
 
+    echo $_Get['show-host-IP'];
     if (isset($_Get['show-host-IP'])){
         $data["settings"]["ros"]["coreIP"] = $_GET['show-host-IP'];
         echo "updated show-host-IP";
     }
+    echo $_GET['show-width'];
     if (isset($_GET['show-width'])){
         $data['display']['width'] = $_GET['show-width'];
         echo "udpated show-width";
