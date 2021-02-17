f541fcc5388073719b02af8f1d86ac9f395ac316
Thomas <Thomas.brefeld@gmail.com>
2021-02-17 16:23:03 -0500

    2

--- src/www/show_form.php
+++ src/www/show_form.php
@@ -5,21 +5,21 @@
 
     echo "test";
 
-    //if (isset($_GET['show-text'])){
-    //    $data["show"]["text"]["msg"] = $_GET['show-text'];
-    //    echo "updated show-text";
-    //}
+    if (isset($_GET['show-text'])){
+        $data["show"]["text"]["msg"] = $_GET['show-text'];
+        echo "updated show-text";
+    }
 
-    //if (isset($_GET['show-status'])){
-    //    $data["show"]["status"]["msg"] = $_GET['show-status'];
-    //    echo "udpated show-status";
-    //}
+    if (isset($_GET['show-status'])){
+        $data["show"]["status"]["msg"] = $_GET['show-status'];
+        echo "udpated show-status";
+    }
 
-    //$jsonData = json_encode($data,JSON_PRETTY_PRINT);
-    //$handle = fopen($file, "w");
-    //if (!fwrite($handle, $jsonData)){
-    //    echo "Failed";
-    //}
-    //fclose($file);
-    //header('Location: http://192.168.1.38/')
+    $jsonData = json_encode($data,JSON_PRETTY_PRINT);
+    $handle = fopen($file, "w");
+    if (!fwrite($handle, $jsonData)){
+        echo "Failed";
+    }
+    fclose($file);
+    header('Location: http://192.168.1.38/')
 ?>
