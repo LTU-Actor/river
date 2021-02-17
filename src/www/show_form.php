5c6d74fb36442488d1f457b70dac1f98201c478c
Thomas <Thomas.brefeld@gmail.com>
2021-02-17 16:22:01 -0500

    2

--- src/www/show_form.php
+++ src/www/show_form.php
@@ -3,23 +3,23 @@
     $strData = file_get_contents($file);
     $data = json_decode($strData, true);
 
-    echo "t";
-    
-    if (isset($_GET['show-text'])){
-        $data["show"]["text"]["msg"] = $_GET['show-text'];
-        echo "updated show-text";
-    }
+    echo "test";
 
-    if (isset($_GET['show-status'])){
-        $data["show"]["status"]["msg"] = $_GET['show-status'];
-        echo "udpated show-status";
-    }
+    //if (isset($_GET['show-text'])){
+    //    $data["show"]["text"]["msg"] = $_GET['show-text'];
+    //    echo "updated show-text";
+    //}
 
-    $jsonData = json_encode($data,JSON_PRETTY_PRINT);
-    $handle = fopen($file, "w");
-    if (!fwrite($handle, $jsonData)){
-        echo "Failed";
-    }
-    fclose($file);
-    header('Location: http://192.168.1.38/')
+    //if (isset($_GET['show-status'])){
+    //    $data["show"]["status"]["msg"] = $_GET['show-status'];
+    //    echo "udpated show-status";
+    //}
+
+    //$jsonData = json_encode($data,JSON_PRETTY_PRINT);
+    //$handle = fopen($file, "w");
+    //if (!fwrite($handle, $jsonData)){
+    //    echo "Failed";
+    //}
+    //fclose($file);
+    //header('Location: http://192.168.1.38/')
 ?>
