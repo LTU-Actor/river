d7c6995cc32f7934358564d5644bb1bc2b25921c
Thomas <Thomas.brefeld@gmail.com>
2021-02-17 15:31:59 -0500

    2

--- src/www/restart_form.php
+++ src/www/restart_form.php
@@ -1,7 +1,7 @@
 <?php
-    echo $_GET['show-host-IP'];
-    echo $_GET['show-width'];
-    echo $_GET['show-height'];
+    echo $_GET['show-host-IP']."<br>";
+    echo $_GET['show-width']."<br>";
+    echo $_GET['show-height']."<br>";
     echo $_GET['show-brightness'];
 
     #header('Location: http://192.168.1.38/')
