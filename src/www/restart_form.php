420db71c268d73216bf1e47ad6abb29a3be081ce
Thomas <Thomas.brefeld@gmail.com>
2021-02-17 15:36:46 -0500

    2

--- src/www/restart_form.php
+++ src/www/restart_form.php
@@ -3,11 +3,16 @@
     $strData = file_get_contents($file);
     $data = json_decode($strData, true);
 
-    
-    echo $_POST['show-host-IP']."<br>";
-    echo $_GET['show-width']."<br>";
-    echo $_GET['show-height']."<br>";
-    echo $_GET['show-brightness'];
+    $data["settings"]["ros"]["coreIP"] = $_GET['show-host-IP'];
+    $data['display']['width'] = $_GET['show-width'];
+    $data['display']['height'] = $_GET['show-height'];
+    $data['display']['brightness'] = $_GET['show-brightness'];
 
+    $jsonData = json_encode($data,JSON_PRETTY_PRINT);
+    $handle = fopen($file, "w");
+    if (!fwrite($handle, $jsonData)){
+        echo "Failed";
+    }
+    fclose($file);
     #header('Location: http://192.168.1.38/')
 ?>
