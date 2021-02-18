97bd33c79730873be3809cf1a3c4643c0bb62f3f
Thomas <Thomas.brefeld@gmail.com>
2021-02-17 22:33:14 -0500

    2

--- src/www/index.php
+++ src/www/index.php
@@ -19,78 +19,82 @@
   ?>
 </head>
 <body>
-<div class="container">
-<h3>Show:</h3>
-<form action="show_form.php">
-  <div class="row">
-		<div class="col-25">
-			<label for="fname">Update text</label>
-		</div>
-		<div class="col-50">
-      <input type=text id="fname" name="show-text" placeholder="<?php echo $data['show']['text']['msg']; ?>">
-		</div>
-    <div class="col-25">
-      <input type="color" id="fcolor" value="#ffffff">
-		</div>
-	</div>
-  <div class="row">
-		<div class="col-25">
-			<label for="fname">Update status</label>
-		</div>
-		<div class="col-50">
-      <input type=text id="fname" name="show-status" placeholder="<?php echo $data['show']['status']['msg']; ?>">
-		</div>
-    <div class="col-25">
-      <input type="color" value="#ff3232">
-		</div>
-	</div>
-  <br>
-	<div class="row">
-		<input type="submit" value="Submit">
-	</div>
-</form>
-</div>
-<br>
-<div class="container">
-  <h3>Settings (Restart Required):</h3>
-	<form action="restart_form.php">
-	<div class="row">
-		<div class="col-25">
-			<label for="fname">Host IP</label>
-		</div>
-		<div class="col-75">
-			<input type=text id="fname" name="show-host-IP" value="<?php echo $data['settings']['ros']['coreIP']; ?>">
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-25">
-			<label for="fname">Display Width</label>
-		</div>
-		<div class="col-75">
-			<input type=text id="fname" name="show-width" value="<?php echo $data['display']['width']; ?>">
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-25">
-			<label for="fname">Display Height</label>
-		</div>
-		<div class="col-75">
-			<input type=text id="fname" name="show-height" value="<?php echo $data['display']['height']; ?>">
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-25">
-			<label for="fname">Display Brightness</label>
-		</div>
-		<div class="col-75">
-			<input type=text id="fname" name="show-brightness" value="<?php echo $data['display']['brightness']; ?>">
-		</div>
-	</div>
-	<br>
-	<div class="row">
-		<input type="submit" value="Submit">
-	</div>
-	</form>
+
+
+<div class="grid-container">
+  <div class="container">
+    <h3>Show:</h3>
+    <form action="show_form.php">
+      <div class="row">
+        <div class="col-25">
+          <label for="fname">Update text</label>
+        </div>
+        <div class="col-50">
+          <input type=text id="fname" name="show-text" placeholder="<?php echo $data['show']['text']['msg']; ?>">
+        </div>
+        <div class="col-25">
+          <input type="color" id="fcolor" value="#ffffff">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-25">
+          <label for="fname">Update status</label>
+        </div>
+        <div class="col-50">
+          <input type=text id="fname" name="show-status" placeholder="<?php echo $data['show']['status']['msg']; ?>">
+        </div>
+        <div class="col-25">
+          <input type="color" value="#ff3232">
+        </div>
+      </div>
+      <br>
+      <div class="row">
+        <input type="submit" value="Submit">
+      </div>
+    </form>
+  </div>
+
+  <div class="container">
+    <h3>Settings (Restart Required):</h3>
+    <form action="restart_form.php">
+    <div class="row">
+      <div class="col-25">
+        <label for="fname">Host IP</label>
+      </div>
+      <div class="col-75">
+        <input type=text id="fname" name="show-host-IP" value="<?php echo $data['settings']['ros']['coreIP']; ?>">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-25">
+        <label for="fname">Display Width</label>
+      </div>
+      <div class="col-75">
+        <input type=text id="fname" name="show-width" value="<?php echo $data['display']['width']; ?>">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-25">
+        <label for="fname">Display Height</label>
+      </div>
+      <div class="col-75">
+        <input type=text id="fname" name="show-height" value="<?php echo $data['display']['height']; ?>">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-25">
+        <label for="fname">Display Brightness</label>
+      </div>
+      <div class="col-75">
+        <input type=text id="fname" name="show-brightness" value="<?php echo $data['display']['brightness']; ?>">
+      </div>
+    </div>
+    <br>
+    <div class="row">
+      <input type="submit" value="Submit">
+    </div>
+    </form>
+  </div>
 </div>
 </body>
 </html>
