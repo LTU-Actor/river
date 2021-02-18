101c47ececbb392b28b67bdeb3e6f1a4ccd12bb2
Thomas <Thomas.brefeld@gmail.com>
2021-02-18 13:24:23 -0500

    2

--- src/www/show_form.php
+++ src/www/show_form.php
@@ -7,12 +7,22 @@
 
     if (!empty($_GET['show-text'])){
         $data["show"]["text"]["msg"] = $_GET['show-text'];
-        echo "updated show-text";
+        echo "updated show-text<br>";
     }
 
     if (!empty($_GET['show-status'])){
         $data["show"]["status"]["msg"] = $_GET['show-status'];
-        echo "udpated show-status";
+        echo "udpated show-status<br>";
+    }
+
+    if (!empty($_GET['show-text-color'])){
+        $data["show"]["text"]["color"] = $_GET['show-text-color'];
+        echo "udpated show-text-color<br>";
+    }
+
+    if (!empty($_GET['show-status-color'])){
+        $data["show"]["status"]["color"] = $_GET['show-status-color'];
+        echo "udpated show-status-color";
     }
 
     $jsonData = json_encode($data,JSON_PRETTY_PRINT);
