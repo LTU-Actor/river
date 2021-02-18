ed522555394eef0237bf0d09a24c6239a17da362
Thomas <Thomas.brefeld@gmail.com>
2021-02-18 14:46:32 -0500

    2

--- src/www/show_form.php
+++ src/www/show_form.php
@@ -22,13 +22,13 @@
         $data["show"]["status"]["color"] = $_GET['show-status-color'];
         echo "udpated show-status-color<br>";
     }
-    echo "<br>".$_GET['toggle-heartbeat']."<br>";
+
     if (!empty($_GET['toggle-heartbeat'])){
-        $data["settings"]["hartbeat"]["enabled"] = $_GET['toggle-heartbeat'];
+        $data["settings"]["hartbeat"]["enabled"] = true;
         echo "toggle-heartbeat true<br>";
     }
     else{
-        $data["settings"]["hartbeat"]["enabled"] = "off";
+        $data["settings"]["hartbeat"]["enabled"] = false;
         echo "toggle-heartbeat false<br>";
     }
 
