0bac2eba60ca6e5f63ac4aa882304334c15dceb3
Thomas <Thomas.brefeld@gmail.com>
2021-02-19 14:12:29 -0500

    2

--- src/start.sh
+++ src/start.sh
@@ -19,6 +19,6 @@ sudo cp "$START_PY_FILE_SRC" "$START_PY_FILE_BIN"
 
 . "$REMOTE_MASTER"
 
-cd "/home/ubuntu/catkin_ws" && catkin_make && source devel/setup.sh
+#cd "/home/ubuntu/catkin_ws" && catkin_make && source devel/setup.sh
 
 sudo python3 "$SHOW_SRC" &
