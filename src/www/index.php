5b25d208355b37799eee2e954b0bba065e05b188
Thomas <Thomas.brefeld@gmail.com>
2021-02-20 15:05:56 -0500

    2

--- src/www/index.php
+++ src/www/index.php
@@ -16,7 +16,7 @@
 <div class="grid-container">
   <div class="container">
     <h3>Defined Settings:</h3>
-	<div class="row">
+	  <div class="row">
       <div class="col-50">
         <label for="rosHostIP">ROS host IP</label>
       </div>
@@ -24,7 +24,7 @@
         <input type=text id="rosHostIP" name="show-text" placeholder="<?php echo $data["settings"]["ros"]["coreIP"]; ?>" disabled>
       </div>
     </div>
-	<div class="row">
+	  <div class="row">
       <div class="col-50">
         <label for="rosPort">ROS port</label>
       </div>
@@ -38,8 +38,8 @@
       </div>
       <div class="col-50">
         <input type=text id="rosRaspberry" name="show-text" placeholder="<?php echo $data["settings"]["ros"]["riverIP"]; ?>" disabled>
+	    </div>
 	  </div>
-	</div>
     <div class="row">
       <div class="col-50">
         <label for="dWidth">Display width</label>
@@ -56,7 +56,7 @@
         <input type=text id="dHeight" name="show-text" placeholder="<?php echo $data["display"]["height"]; ?>" disabled>
       </div>
     </div>
-	<div class="row">
+	  <div class="row">
       <div class="col-50">
         <label for="dBrightness">Display brightness</label>
       </div>
@@ -91,17 +91,57 @@
           <input type="color" id="UStatus" name="show-status-color" value="<?php echo $data['show']['status']['color']; ?>">
         </div>
       </div>
-	  <div class="row">
+	    <div class="row">
         <div class="col-75">
           <label for="Theartbeat">Toggle heartbeat</label>
-		  <input type=checkbox id="Theartbeat" name="toggle-heartbeat" value="true" <?php if ($data['settings']['hartbeat']['enabled']){echo checked;} ?>>
+		      <input type=checkbox id="Theartbeat" name="toggle-heartbeat" value="true" <?php if ($data['settings']['hartbeat']['enabled']){echo checked;} ?>>
         </div>
-		<div class="col-25">
+		    <div class="col-25">
           <input type="color" id="Theartbeat" name="heartbeat-color" value="<?php echo $data['settings']['hartbeat']['color']; ?>">
         </div>
       </div>
       <br>
 
+      <div class="row">
+        <input type="submit" value="Submit">
+      </div>
+    </form>
+  </div>
+
+  <div class="container">
+    <h3>auto:</h3>
+    <form action="auto_form.php">
+      <div class="row">
+        <div class="col-75">
+          <label for="TAutoMode">Toggle automatic mode</label>
+		      <input type=checkbox id="TAutoMode" name="toggle-auto-mode" value="true" <?php if ($data['auto']['enabled']){echo checked;} ?>>
+        </div>
+      <div class="row">
+        <div class="col-25">
+          <label for="DLevel">Debug Level</label>
+        </div>
+        <div class="col-75">
+          <input type=text id="DLevel" name="show-debug-level" value="<?php echo $data['auto']['level']; ?>">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-25">
+          <label for="Duration">Duration</label>
+        </div>
+        <div class="col-75">
+          <input type=text id="Duration" name="show-duration" value="<?php echo $data['auto']['duration']; ?>">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-25">
+          <label for="Timeout">Timeout</label>
+        </div>
+        <div class="col-75">
+          <input type=text id="Timeout" name="show-timeout" value="<?php echo $data['auto']['timeout']; ?>">
+        </div>
+      </div>
+
+      <br>
       <div class="row">
         <input type="submit" value="Submit">
       </div>
@@ -119,7 +159,7 @@
         <input type=text id="HostIP" name="show-host-IP" value="<?php echo $data['settings']['ros']['coreIP']; ?>">
       </div>
     </div>
-	<div class="row">
+	  <div class="row">
       <div class="col-25">
         <label for="Port">ROS port</label>
       </div>
