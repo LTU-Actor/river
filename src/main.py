05bc994cf901fdebb5dabe2937978387abb167c1
Thomas <Thomas.brefeld@gmail.com>
2021-02-20 12:19:56 -0500

    2

--- src/main.py
+++ src/main.py
@@ -8,31 +8,22 @@ import logging
 from std_msgs.msg import String
 from std_msgs.msg import Int8
 from rosgraph_msgs.msg import Log
+from std_msgs.msg import Bool
 
 dataFile = "/home/ubuntu/catkin_ws/src/river/src/data.json"
 logFile = "/home/ubuntu/catkin_ws/src/river/src/main.log"
 
 lastUpdate = None
 data = None
-text = None
-status = None
-log = None
 
 assert os.path.exists(dataFile), 'No such file: \'data.json\''
 
 def update():
 	global lastUpdate
 	global data
-	global text
-	global status
-
-	print("Update!")
 
 	with open(dataFile) as file:
 		data = json.load(file)
-
-		text = data["show"]["text"]["msg"]
-		status = data["show"]["status"]["msg"]
 
 	lastUpdate = pathlib.Path(dataFile).stat().st_mtime
 
@@ -57,15 +48,46 @@ def statusCB(msg):
 	update()
 
 def logCB(msg):
-	print([msg.level, msg.header.stamp.to_sec(), msg.name, msg.topics, msg.msg])
+	data["auto"]["data"]["msg"].append(
+		[msg.level, 
+		msg.header.stamp.to_sec(), 
+		msg.name, 
+		msg.topics, 
+		msg.msg])
+
+	jsonObj = json.dumps(data, indent = 4)
+
+	with open(dataFile, "w") as file:
+		file.write(jsonObj)
+
+	update()
+
+def enabledCB(msg):
+	data["auto"]["data"]["dbw_enabled"] = msg.data
+
+	jsonObj = json.dumps(data, indent = 4)
+
+	with open(dataFile, "w") as file:
+		file.write(jsonObj)
+
+	update()
 
 if __name__ == '__main__':
 	rospy.init_node('display_node')
-	print('display_node')
+
+	update()
+	data["auto"]["data"]["msg"] = []
+
+	jsonObj = json.dumps(data, indent = 4)
+	
+	with open(dataFile, "w") as file:
+		file.write(jsonObj)
+
 
 	rospy.Subscriber("display/text", String, textCB)
 	rospy.Subscriber("display/status", Int8, statusCB)
 	rospy.Subscriber("rosout", Log, logCB)
+	rospy.Subscriber("/vehicle/dbw_enabled", Bool, enabledCB)
 
 	rate = rospy.Rate(10)
 	while not rospy.is_shutdown():
