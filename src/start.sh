6ada52e166da92e65b4851891182f8728e455903
Thomas <Thomas.brefeld@gmail.com>
2021-02-23 16:42:16 -0500

    2

--- src/start.sh
+++ src/start.sh
@@ -2,10 +2,10 @@ REMOTE_MASTER=/home/ubuntu/catkin_ws/src/river/src/remote-master.sh
 DATA_JSON_SRC=/home/ubuntu/catkin_ws/src/river/src/data.json
 DATA_JSON_TEMP=/home/ubuntu/catkin_ws/src/river/src/dataTemp.json
 
-if test -f "$DATA_JSON_TEMP";
-then
-        sudo rm -rf "$DATA_JSON_TEMP"
-fi
+#if test -f "$DATA_JSON_TEMP";
+#then
+#        sudo rm -rf "$DATA_JSON_TEMP"
+#fi
 
 sudo cp "$DATA_JSON_SRC" "$DATA_JSON_TEMP"
 
