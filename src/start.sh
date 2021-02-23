6b39521e3d0a72d0fddb9e006534185140a4ce07
Thomas <Thomas.brefeld@gmail.com>
2021-02-23 15:23:53 -0500

    2

--- src/start.sh
+++ src/start.sh
@@ -5,39 +5,34 @@ REMOTE_MASTER=/home/ubuntu/catkin_ws/src/river/src/remote-master.sh
 WEBSITE_SRC=/home/ubuntu/catkin_ws/src/river/src/www
 DATA_JSON_SRC=/home/ubuntu/catkin_ws/src/river/src/data.json
 
-networkConnected=0
-
-for ((i = 0; i<10; ++i)); do
-  [ "$(hostname -I)" = "" ] && networkConnected=1 && echo "Network found!" && break
-  echo "No network"
+while [ "$(hostname -I)" = "" ]; do
+  echo -e "\e[1A\e[KNo network: $(date)"
   sleep 1
 done
 
-if [[ $networkConnected == 1]]; do
-  cd "/home/ubuntu/catkin_ws/src/river" && git pull
+cd "/home/ubuntu/catkin_ws/src/river" && git pull
 
-  if test -f "$START_PY_FILE_BIN";
-  then
-    sudo rm -rf "$START_PY_FILE"
-  fi
-
-  sudo cp "$START_PY_FILE_SRC" "$START_PY_FILE_BIN"
-
-  if [ -d "/var/www" ];
-  then
-    sudo rm -rf "/var/www"
-  fi
-
-  sudo cp -R "$WEBSITE_SRC" "/var/www"
-  sudo chmod -R 775 "/var/www"
-  sudo chmod 777 "$DATA_JSON_SRC"
-
-  systemctl daemon-reload
-  sudo systemctl restart apache2
-
-  sudo apt-get install ros-kinetic-catkin
-
-  . "$REMOTE_MASTER"
-
-  cd "/home/ubuntu/catkin_ws" && catkin_make && source devel/setup.sh
+if test -f "$START_PY_FILE_BIN";
+then
+  sudo rm -rf "$START_PY_FILE"
 fi
+
+sudo cp "$START_PY_FILE_SRC" "$START_PY_FILE_BIN"
+
+if [ -d "/var/www" ];
+then
+  sudo rm -rf "/var/www"
+fi
+
+sudo cp -R "$WEBSITE_SRC" "/var/www"
+sudo chmod -R 775 "/var/www"
+sudo chmod 777 "$DATA_JSON_SRC"
+
+systemctl daemon-reload
+sudo systemctl restart apache2
+
+sudo apt-get install ros-kinetic-catkin
+
+. "$REMOTE_MASTER"
+
+cd "/home/ubuntu/catkin_ws" && catkin_make && source devel/setup.sh
