82748220d338c6d0cb82b806cb0ccd7b0bd4aec6
Thomas <Thomas.brefeld@gmail.com>
2021-02-23 17:54:33 -0500

    2

--- src/www/update.php
+++ src/www/update.php
@@ -1,4 +1,5 @@
 <?php
-    $output = shell_exec('cd /home/ubuntu/catkin_ws/src/river/src/ && sudo ./update.sh 2>&1');
-    echo "$output";
+    shell_exec('cd /home/ubuntu/catkin_ws/src/river/src/ && sudo ./update.sh 2>&1');
+    
+    header('Location: http://192.168.1.38/');
 ?>
