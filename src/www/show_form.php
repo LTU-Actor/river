31d32eaeb9a2a6ddb8398cd71f94668f6bc3098c
Thomasbrefeld <thomas.brefeld@gmail.com>
2021-02-23 19:50:35 -0500

    Update show_form.php

--- src/www/show_form.php
+++ src/www/show_form.php
@@ -66,5 +66,5 @@
 
     shell_exec('cd /home/ubuntu/catkin_ws/src/river/src/ && sudo ./start.sh 2>&1');
 
-    header('Location: http://192.168.1.38/')
+    header('Location: http://192.168.0.96/')
 ?>
