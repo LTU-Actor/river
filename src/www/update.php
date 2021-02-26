b4ddf557c5e331e837df04536fcf482842787bb1
Thomas <Thomas.brefeld@gmail.com>
2021-02-26 12:22:43 -0500

    2

--- src/www/update.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $output = shell_exec('cd /home/ubuntu/catkin_ws/src/river/src/ && sudo ./update.sh 2>&1');
-    if (isset($_SERVER["HTTP_REFERER"])) {
-        header("Location: " . $_SERVER["HTTP_REFERER"]);
-    }
-?>
