4953538a5a941afe5f473e3d42e86b4d8c6d75e5
Thomas <Thomas.brefeld@gmail.com>
2021-02-26 16:01:40 -0500

    1

--- src/www/index.php
+++ src/www/index.php
@@ -13,10 +13,10 @@
 <body>
   <div class="grid-container">
     <div class="container">
-      <h3>Defined Settings:</h3>
+      <h3>Settings:</h3>
       <div class="row">
         <div class="col-50">
-          <label>ROS host IP</label>
+          <label>ROS host</label>
         </div>
         <div class="col-50">
           <input type="text" placeholder="<?php echo $data["settings"]["ros"]["coreIP"].":".$data["settings"]["ros"]["port"]; ?>" disabled>
@@ -40,14 +40,6 @@
       </div>
       <div class="row">
         <div class="col-50">
-          <label>Display height</label>
-        </div>
-        <div class="col-50">
-          <input type="text" placeholder="<?php echo $data["display"]["height"]; ?>" disabled>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-50">
           <label>Display brightness</label>
         </div>
         <div class="col-50">
@@ -55,5 +47,155 @@
         </div>
       </div>
     </div>
+
+    <div class="container">
+      <h3>Show:</h3>
+      <form name="show_form" action="show_form.php" onsubmit="return validateShowForm()">
+        <div class="row">
+          <div class="col-25">
+            <label>Update text</label>
+          </div>
+          <div class="col-50">
+            <input type=text name="show-text" placeholder="<?php echo $data['show']['text']['msg']; ?>">
+          </div>
+          <div class="col-25">
+            <input type="color"name="show-text-color" value="<?php echo $data['show']['text']['color']; ?>">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-25">
+            <label>Update status</label>
+          </div>
+          <div class="col-50">
+            <input type=text name="show-status" value="<?php echo $data['show']['status']['msg']; ?>">
+          </div>
+          <div class="col-25">
+            <input type="color" name="show-status-color" value="<?php echo $data['show']['status']['color']; ?>" <?php if ($data['show']['status']['colorGrade']){echo disabled;} ?>>
+          </div>
+        </div>
+        <div class="row">
+          <label>Toggle heartbeat</label>
+          <input type=checkbox name="toggle-heartbeat" value="true" <?php if ($data['settings']['hartbeat']['enabled']){echo checked;} ?>>
+        </div>
+        <div class="row">
+          <div class="col-75">
+            <label>Toggle clear zero</label>
+            <input type=checkbox name="toggle-clear-zero" value="true" <?php if ($data['show']['status']['clear0']){echo checked;} ?>>
+          </div>
+        </div>
+
+        <br>
+
+        <div class="row">
+          <input type="submit" value="Submit">
+        </div>
+      </form>
+    </div>
+
+    <div class="container">
+      <h3>Automatic mode:</h3>
+      <form name="auto_form" action="auto_form.php" onsubmit="return validateAutoForm()">
+        <div class="row">
+          <div class="col-75">
+            <label>Toggle automatic mode</label>
+            <input type=checkbox name="toggle-auto-mode" value="true" <?php if ($data['auto']['enabled']){echo checked;} ?>>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-25">
+            <label>Debug Level</label>
+          </div>
+          <div class="col-75">
+            <input type=text name="show-debug-level" value="<?php echo $data['auto']['level']; ?>">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-25">
+            <label>Duration</label>
+          </div>
+          <div class="col-75">
+            <input type=text name="show-duration" value="<?php echo $data['auto']['duration']; ?>">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-25">
+            <label>Timeout</label>
+          </div>
+          <div class="col-75">
+            <input type=text name="show-timeout" value="<?php echo $data['auto']['timeout']; ?>">
+          </div>
+        </div>
+
+        <br>
+        <div class="row">
+          <input type="submit" value="Submit">
+        </div>
+      </form>
+    </div>
+
+    <div class="container">
+      <h3>Network and Updates:</h3>
+      <form name="update" action="update.php">
+      <div class="row">
+        <div class="col-75">
+          <label>Force Github update</label>
+        </div>
+        <div class="col-25">
+          <input type="submit" value="Update ROS River">
+        </div>
+      </div>
+      </form>
+    </div>
+
+    <div class="container">
+      <h3>Settings (Restart Required):</h3>
+      <form name="restart_form" action="restart_form.php" onsubmit="return validateRestartForm()">
+      <div class="row">
+        <div class="col-25">
+          <label>Host IP</label>
+        </div>
+        <div class="col-75">
+          <input type=text name="show-host-IP" value="<?php echo $data['settings']['ros']['coreIP']; ?>">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-25">
+          <label>ROS port</label>
+        </div>
+        <div class="col-75">
+          <input type=text name="show-port" value="<?php echo $data['settings']['ros']['port']; ?>">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-25">
+          <label>Display Width</label>
+        </div>
+        <div class="col-75">
+          <input type=text name="show-width" value="<?php echo $data['display']['width']; ?>">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-25">
+          <label>Display Height</label>
+        </div>
+        <div class="col-75">
+          <input type=text name="show-height" value="<?php echo $data['display']['height']; ?>">
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-25">
+          <label>Display Brightness</label>
+        </div>
+        <div class="col-75">
+          <input type=text name="show-brightness" value="<?php echo $data['display']['brightness']; ?>">
+        </div>
+      </div>
+      <br>
+      <div class="row">
+        <input type="submit" value="Submit and Reboot">
+      </div>
+      </form>
+    </div>
+
   </div>
 </body>
