513d1bce7e768246a7e97288f00dbb49bea5f478
Thomas <Thomas.brefeld@gmail.com>
2021-02-26 20:44:55 -0500

    2

--- src/www/display_form.php
+++ src/www/display_form.php
@@ -17,7 +17,9 @@
     }
     fclose($file);
 
-    #if (isset($_SERVER["HTTP_REFERER"])) {
-    #    header("Location: " . $_SERVER["HTTP_REFERER"]);
-    #}
+    shell_exec('cd /home/ubuntu/catkin_ws/src/river/src/ && sudo ./start.sh 2>&1');
+
+    if (isset($_SERVER["HTTP_REFERER"])) {
+        header("Location: " . $_SERVER["HTTP_REFERER"]);
+    }
 ?>
