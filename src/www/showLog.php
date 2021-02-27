aae448debacc811da1131284fc44d8c7d7d3b4eb
Thomas <Thomas.brefeld@gmail.com>
2021-02-27 12:49:45 -0500

    2

--- src/www/showLog.php
+++ src/www/showLog.php
@@ -1,5 +1,8 @@
 <?php
     $myfile = fopen("/home/ubuntu/catkin_ws/src/river/src/show.log", "r") or die("Unable to open file!");
-    echo fread($myfile,filesize("webdictionary.txt"));
+    // Output one line until end-of-file
+    while(!feof($myfile)) {
+    echo fgets($myfile) . "<br>";
+    }
     fclose($myfile);
 ?>
