5aa68ae2ded917d21cb51427b06640d94412dcc9
Thomas <Thomas.brefeld@gmail.com>
2021-02-27 12:58:00 -0500

    2

--- src/www/showLog.php
+++ src/www/showLog.php
@@ -1,8 +1,16 @@
+<?php
+    $myfile = fopen("/home/ubuntu/catkin_ws/src/river/src/show.log", "r") or die("Unable to open file!");
+    while(!feof($myfile)) {
+    echo fgets($myfile) . "<br>";
+    }
+    fclose($myfile);
+?>
 
 <?php 
     $linecount = 0;
     $myfile = fopen("/home/ubuntu/catkin_ws/src/river/src/show.log", "r");
     while(!feof($myfile)) {
+        fgets($myfile) 
         $linecount++;
     }
     fclose($myfile);
