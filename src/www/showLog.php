945ba297303e1b60713b9843a44736e465a30215
Thomas <Thomas.brefeld@gmail.com>
2021-02-27 12:57:01 -0500

    2

--- src/www/showLog.php
+++ src/www/showLog.php
@@ -1,10 +1,3 @@
-<?php
-    $myfile = fopen("/home/ubuntu/catkin_ws/src/river/src/show.log", "r") or die("Unable to open file!");
-    while(!feof($myfile)) {
-    echo fgets($myfile) . "<br>";
-    }
-    fclose($myfile);
-?>
 
 <?php 
     $linecount = 0;
