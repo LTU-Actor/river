7cf724b080a17c417509e28f3b82d21f4ac8cc6f
Thomas <Thomas.brefeld@gmail.com>
2021-02-27 14:05:04 -0500

    2

--- src/remote-master.sh
+++ src/remote-master.sh
@@ -1,7 +1,14 @@
-CORE_IP= jq .settings.ros.coreIP /home/ubuntu/catkin_ws/src/river/src/data.json
-PORT= jq .settings.ros.port /home/ubuntu/catkin_ws/src/river/src/data.json
-RIVER_IP= jq .settings.ros.riverIP /home/ubuntu/catkin_ws/src/river/src/data.json
-echo CORE_IP.PORT
-#export ROS_MASTER_URI=http://192.168.1.32:11311
-#export ROS_IP=192.168.1.38
+CORE_IP=$( jq .settings.ros.coreIP /home/ubuntu/catkin_ws/src/river/src/data.json )
+PORT=$( jq .settings.ros.port /home/ubuntu/catkin_ws/src/river/src/data.json)
+RIVER_IP=$( jq .settings.ros.riverIP /home/ubuntu/catkin_ws/src/river/src/data.json)
+
+CORE_IP="${CORE_IP%\"}"
+CORE_IP="${CORE_IP#\"}"
+PORT="${PORT%\"}"
+PORT="${PORT#\"}"
+RIVER_IP="${RIVER_IP%\"}"
+RIVER_IP="${RIVER_IP#\"}"
+
+export "ROS_MASTER_URI=http://$CORE_IP:$PORT"
+export "ROS_IP=$RIVER_IP"
 echo "ROS_MASTER_URI and ROS_IP updated"
